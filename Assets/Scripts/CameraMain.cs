160937ddfd4dcf6f21079df2b5614c080a917d18
Nakul Iyer <nakulpiyer@gmail.com>
2020-12-22 04:08:24 -0500

    first commit

---
+++ Assets/Scripts/CameraMain.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMain : MonoBehaviour
+{
+    /* Objects */
+    public GameObject player;
+
+    /* Animation Time */
+    [Serializable]
+    public enum AnimationMode
+    {
+        kZooming,
+        kTracking
+    }
+    public float zoomAnimationTime = 1.5f;
+    public float trackAnimationTime = 3.0f;
+    public AnimationMode initialAnimationMode = AnimationMode.kZooming;
+    public AnimationMode defaultAnimationMode = AnimationMode.kTracking;
+
+    /* Movement */
+    public Vector3 optimalDistanceToPlayer = new Vector3(0, 4.0f, -20.0f);
+    public float minAnimationDistance = 0.5f;
+
+    /* Rotation */
+    public float rotationSpeed = 0.2f;
+    public float rotationDampening = 0.4f;
+
+    /* Privates */
+    private bool isMoving = false;
+    private AnimationMode animationMode;
+    private (Vector3, Vector3) animationEndpoints;
+    private uint currentFrame;
+    private uint totalFrames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationEndpoints = (Vector3.zero, transform.position);
+        animationMode = initialAnimationMode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player.GetComponent<PlayerMain>().hitBottom)
+        {
+            return;
+        }
+
+        float playerY = player.transform.position.y;
+        Vector3 optimalPosition = new Vector3(0, playerY, 0) + optimalDistanceToPlayer;
+        if ((!isMoving || animationMode == AnimationMode.kTracking) &&
+            Vector3.Distance(animationEndpoints.Item2, optimalPosition) > minAnimationDistance)
+        {
+            isMoving = true;
+            animationEndpoints = (transform.position, optimalPosition);
+            currentFrame = 0;
+
+            if (animationMode == AnimationMode.kZooming)
+            {
+                totalFrames = (uint)(zoomAnimationTime / Time.deltaTime);
+            }
+            else if (animationMode == AnimationMode.kTracking)
+            {
+                totalFrames = (uint)(trackAnimationTime / Time.deltaTime);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            AnimatePosition();
+        }
+        AnimateRotation();
+    }
+
+    private void AnimatePosition()
+    {
+        // Debug.Log("animating from " + animationEndpoints.Item1 + " to " + animationEndpoints.Item2 + " at frame " + currentFrame + " of " + totalFrames);
+        if (currentFrame < totalFrames)
+        {
+            float frameCoverage = Mathf.Sin(Mathf.PI / 2 * currentFrame / totalFrames);
+            transform.position = animationEndpoints.Item1 + frameCoverage * (animationEndpoints.Item2 - animationEndpoints.Item1);
+            ++currentFrame;
+        }
+        else
+        {
+            isMoving = false;
+            animationMode = defaultAnimationMode;
+        }
+    }
+
+    private void AnimateRotation()
+    {
+        Vector3 rotationDirection = player.transform.position - transform.position;
+        rotationDirection.y = 0; // keep strictly horizontal
+        Quaternion q = Quaternion.LookRotation(rotationDirection * rotationDampening);
+        float rotationStep = rotationSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, rotationStep);
+    }
+}
---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour
+{
+    /* Truly public */
+    public bool hitBottom = false;
+
+    /* Movement */
+    public float jumpWaitTime = 0.5f;
+    public float upwardsThrust = 6.0f;
+    public float upwardsThrustDegradation = 1.0f;
+    public float directionalThrust = 3.0f;
+    public float directionalThrustDegradation = 0.3f;
+    public uint maxJumpCount = 8;
+
+    /* Component */
+    private Rigidbody rb;
+
+    /* Player variables */
+    private bool isOnGround = false;
+    private float lastJumpTime = 0f;
+    private uint jumpCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastJumpTime > jumpWaitTime)
+        {
+            UseKeyboard();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float colliderY = collision.gameObject.transform.position.y;
+        float squashDistance = transform.localScale.y / 2;
+
+        bool somethingAbove = colliderY - transform.position.y >= squashDistance;
+        bool somethingBelow = transform.position.y - colliderY >= squashDistance;
+
+        if (somethingAbove && isOnGround)
+        {
+            OnSquashed();
+        } else if (somethingBelow)
+        {
+            OnHitGround();
+        }
+    }
+
+    /** Uses the Keyboard as a placeholder for mobile input */
+    private void UseKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(Vector3.zero);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump(Vector3.forward);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Jump(Vector3.back);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Jump(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Jump(Vector3.right);
+        }
+    }
+
+    private void Jump(Vector3 horizontalDirection)
+    {
+        if (jumpCount < maxJumpCount)
+        {
+            float actualUpwardsThrust = upwardsThrust - jumpCount * upwardsThrustDegradation;
+            float actualDirectionalThrust = directionalThrust - jumpCount * directionalThrustDegradation;
+            rb.velocity = Vector3.up * actualUpwardsThrust + horizontalDirection * actualDirectionalThrust;
+            lastJumpTime = Time.time;
+            ++jumpCount;
+            isOnGround = false;
+        }
+    }
+
+    private void OnSquashed()
+    {
+        Debug.Log("squashed");
+    }
+
+    private void OnHitGround()
+    {
+        jumpCount = 0;
+        isOnGround = true;
+        hitBottom = true;
+    }
+}
